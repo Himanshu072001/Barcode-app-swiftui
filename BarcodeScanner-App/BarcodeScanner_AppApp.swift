bb43c9dddbdf46dee79cd45b6b3d160aa8577f08
Himanshu Kesharwani <>
2024-07-25 12:03:00 +0530

    [ADD]: Initail project setup commit.

---
+++ BarcodeScanner-App/BarcodeScanner_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScanner_AppApp.swift
+//  BarcodeScanner-App
+//
+//  Created by Himanshu Kesharwani on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScanner_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeScanner-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarcodeScanner-App
+//
+//  Created by Himanshu Kesharwani on 25/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
